070950b2d7d508027488b4a60553d81dadb895a5
kabrini <kgganier@gmail.com>
2024-07-30 20:37:45 -0500

    update dev tools day 1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Error 1:
 let message = 'Welcome to the debugging exercise!';
-console.log(mesage); 
+console.log(message); 
 
 // Error 2: 
-let button = document.querySelector('.actionButton'); 
-button.addEventListener('click', eventHandler); 
+let button = document.querySelector('#actionButton'); 
+actionButton.addEventListener('click', eventHandler); 
 
 // Error 3: 
 function eventHandler() {
@@ -21,9 +21,10 @@ function displayOutput() {
 // Error 6: Misusing variable scope
 function updateMessage() {
   let localMessage = 'Updated message';
+  console.log(localMessage);
 }
 updateMessage();
-console.log(localMessage); 
+
 
 // Error 7:
 document.getElementById('testButton').addEventListener('click', testFunction());
@@ -39,11 +40,11 @@ console.log('Sum is: ' + (number1 - number2));
 
 // Error 9: 
 let nullVar = null;
-console.log(nullVar.length);
 
+console.log(nullVar?.length);
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
@@ -51,22 +52,24 @@ let importantData =
 console.log('importantData');
 
 // Error 12: 
-tryToCallFunction();
+let tryToCallFunction;
+
+console.log(tryToCallFunction);
 
 // Error 13:
 let user = { namee: 'Alice' };
-console.log(user.name)
+console.log(user.namee);
 
 // Error 14: 
 let someNumber = 123;
-console.log(someNumber.toUpperCase()); 
+console.log(someNumber); 
 
 // Error 15: 
-let greeting = Hello, world!;
+let greeting = "Hello, world!";
 
 // Error 16: 
-for (let i = 0; i < 5; i++); {
-    console.log(i);
+for (let i = 0; i < 5; i++) {
+  console.log(i);
 }
 
 // Error 17:
@@ -75,18 +78,18 @@ if (five === 5) {
     console.log('Five is equal to 5'); // This one should be logged
 } else {
     console.log('Five is not equal to 5'); 
-
+}
 // Error 18:
 Array.prototype.push = function() {
     console.log('Array push method is overwritten');
-};
+}
 let numbers = [];
 numbers.push(1);
 console.log(numbers);
 
 // Error 19:
-console.log(x);
-x = 5;
+let x = 5;
+console.log(x); 
 
 // Error 20:
 let myString = 'Hello';
